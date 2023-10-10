fccc4fcc2b2fb7b0f03fb27f0c2ad4951d006572
Patricklt2 <ptorlaschis@gmail.com>
2023-10-09 22:54:39 -0300

    Se implementa un memory manager rudimentario

--- Kernel/include/memoryManager.h
+++ Kernel/include/memoryManager.h
@@ -1,9 +1,11 @@
-#define size_t unsigned long int
+#include <stddef.h>
 #define PAGE_SIZE 512
 
 //La memoria se definio en el .c pues donde arranca y termina la memoria no deberia ser visible para tod el so, sino para el MM
 
-typedef struct MemoryManagerCDT * MemoryManagerADT;
+void createMemoryManager();
 
-MemoryManagerADT createMemoryManager(void *const restrict memForMM, void *const restrict memToManage);
-void *allocMemory(MemoryManagerADT const restrict memManager, const size_t memBlockSize);
+void * allocate(size_t size);
+
+void deallocate(void * addr);
+
--- Kernel/memoryManager.c
+++ Kernel/memoryManager.c
@@ -1,6 +1,90 @@
 #include "./memoryManager.h"
 
-//64 KB libres
+//INTENTO DE ALOCAR MEMORIA ESTATICA DE MANERA DINAMICA (Para un manipular mas facilmente los strings)
+#define MEMORY_SIZE 65536
 #define MEM_START_ADR 0x0000000000050000
-#define MEM_END_ADR 0x000000000009FFFF
+/*
+ * Struct -> Define bloques que tienen como dato:
+ * El espacio que ocupan.
+ * Si esta siendo utilizado.
+ * Un puntero al siguiente en la lista.
+ */
 
+typedef struct BlockHeader {
+    size_t size;
+    size_t is_free;
+    struct BlockHeader* next;
+} BlockHeader;
+
+
+//Puntero estatico al comienzo del bloque de memoria
+static BlockHeader* first_block;
+
+void createMemoryManager(){
+    // Inicializo la cabecera del bloque haciendo que apunte al comienzo del array memory
+    first_block = (BlockHeader*)MEM_START_ADR;
+    first_block->size = MEMORY_SIZE - sizeof(BlockHeader);
+    first_block->is_free = 1;
+    first_block->next = NULL;
+}
+
+void* allocate(size_t size) {
+    // Ahora la idea es buscar un bloque con suficiente espacio
+    BlockHeader* curr_block = first_block;
+    while (curr_block) {
+        if (curr_block->is_free && curr_block->size >= size) {
+
+            // Si el bloque es mas grande de lo necesario, reservo el espacio y dejo el resto del espacio libre;
+            if (curr_block->size >= size + sizeof(BlockHeader)) {
+
+                BlockHeader* new_block = (BlockHeader*)((char*)curr_block + sizeof(BlockHeader) + size);    //puntero al nuevo bloque va a estar en la posicion curr_block + el size del struct + lo que quiero reservar de memoria
+
+                //new_block reduce su size en size y sizeof(BlockHeader), esta libre, y su next es el next del current block
+                new_block->size = curr_block->size - size - sizeof(BlockHeader);
+                new_block->is_free = 1;
+                new_block->next = curr_block->next;
+
+
+                //current block toma el size pedido, se reserva y el siguiente es el new block
+                curr_block->size = size;
+                curr_block->is_free = 0;
+                curr_block->next = new_block;
+
+            }
+            else {  //tiene el espacio justo para este bloque => lo reservo
+                curr_block->is_free = 0;
+            }
+
+            // Retorno un puntero a esta seccion del bloque
+            return (void*)((char*)curr_block + sizeof(BlockHeader));
+        }
+        //si no hay espacio sigo al siguiente
+        curr_block = curr_block->next;
+    }
+
+    // No hay bloques de memoria disponibles con suficiente espacio
+    return NULL;
+}
+
+void deallocate(void* ptr) {
+    if (ptr == NULL) {
+        return;  // Si apunta a nada => no hay nada que liberar
+    }
+
+    // Obtengo el puntero al bloque que quiero liberar y cambio el flag is_free a 1
+    BlockHeader* block = (BlockHeader*)((char*)ptr - sizeof(BlockHeader));
+    block->is_free = 1;
+
+    // Fusion de los bloques consecutivos que esten libres
+    BlockHeader* curr_block = first_block;    //recorro desde el principio
+    while (curr_block) {
+        if (curr_block->is_free) {
+            if (curr_block->next && curr_block->next->is_free) {
+                curr_block->size += sizeof(BlockHeader) + curr_block->next->size;
+                curr_block->next = curr_block->next->next;
+            }
+        }
+        //paso al siguiente
+        curr_block = curr_block->next;
+    }
+}
