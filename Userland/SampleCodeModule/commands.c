d8a496b9f99bccb88d65a46ef43c7fc1aa42181e
Tim S <84805291+tm-sm@users.noreply.github.com>
2023-10-29 15:13:25 -0300

    improves the shell's command parser

--- Userland/SampleCodeModule/commands.c
+++ Userland/SampleCodeModule/commands.c
@@ -3,98 +3,155 @@
 #include <graphics.h>
 #include <timeDate.h>
 #include <system.h>
+#include <commands.h>
 
+#define ARGS int argc, char* argv[]
 
-typedef void (*FunctionPtr)();
+typedef int (*functionPtr)(ARGS);
 
-struct PROGRAM_STRUCT {
-    char* name_id;
+struct EXECUTABLE {
+    char* name;
     char* description;
-    FunctionPtr program;
-}PROGRAM_STRUCT;
+    functionPtr program;
+}EXECUTABLE;
 
-typedef struct PROGRAM_STRUCT* Program;
+
+typedef struct EXECUTABLE* exec;
 
 extern void invalidOp();
 
 void unknownCommand(char* str);
 
-void help(), testException0(),testException6(), displayTime(), displayDate(), playBubbles(), playPong(), playBeep();
+int help(ARGS), testException0(ARGS),testException6(ARGS), displayTime(ARGS), displayDate(ARGS),
+sh(ARGS), playBubbles(ARGS), playPong(ARGS), playBeep(ARGS);
 
-static Program pArr[] = {
-        &(struct PROGRAM_STRUCT){"help", "displays all available commands", help},
-        &(struct PROGRAM_STRUCT){"test-div0", "runs a zero division", testException0},
-        &(struct PROGRAM_STRUCT){"test-invalidop", "runs an invalid op", testException6},
-        &(struct PROGRAM_STRUCT){"time", "prints the current time", displayTime},
-        &(struct PROGRAM_STRUCT){"date", "prints the current date", displayDate},
-        &(struct PROGRAM_STRUCT){"bubbles", "shows colored bubbles on the screen", playBubbles},
-        &(struct PROGRAM_STRUCT){"pong", "runs a virtual ping pong match against the computer", playPong},
-        &(struct PROGRAM_STRUCT){"beep", "produces a 'beep' sound", playBeep},
-        null //marks the end of the array
+static exec bArr[] = {
+        &(struct EXECUTABLE){"help", "displays all available commands", help},
+        &(struct EXECUTABLE){"test-div0", "runs a zero division", testException0},
+        &(struct EXECUTABLE){"test-invalidop", "runs an invalid op", testException6},
+        &(struct EXECUTABLE){"time", "prints the current time", displayTime},
+        &(struct EXECUTABLE){"date", "prints the current date", displayDate},
+        &(struct EXECUTABLE){"sh", "runs the specified process", sh},
+        NULL
         };
 
-int parseCommand(char* str) {
-    for(int i=0;pArr[i]!=null;i++){
-        if(compString(str,pArr[i]->name_id)==0){
-            return createProcess(pArr[i]->program, HIGH, FOREGROUND, pArr[i]->name_id);
+static exec pArr[] = {
+        &(struct EXECUTABLE){"bubbles", "shows colored bubbles on the screen", playBubbles},
+        &(struct EXECUTABLE){"pong", "runs a virtual ping pong match against the computer", playPong},
+        &(struct EXECUTABLE){"beep", "produces a 'beep' sound", playBeep},
+        NULL
+
+};
+
+int callBuiltin(int argc, char* argv[]) {
+    if(argc >= 1) {
+        for(int i=0; bArr[i]!=NULL; i++) {
+            if(strcmp(bArr[i]->name, argv[0]) == 0) {
+                return bArr[i]->program(argc, argv);
+            }
         }
     }
-    unknownCommand(str);
-    return -1;
+    return 0;
+}
+
+int parseCommand(char* str) {
+    if (str == NULL) {
+        return -1;
+    }
+
+    int ret = -1;
+    char** commands = (char**)alloc(sizeof(char*) * (MAX_PARAMS + 1));
+    int i = 0;
+    char *token = strtok(str, " ");
+
+    while (token != NULL && i < MAX_PARAMS) {
+        commands[i] = (char *)alloc(strlen(token) + 1);
+        strcpy(commands[i], token);
+        i++;
+        token = strtok(NULL, " ");
+    }
+    commands[i] = NULL;
+
+    ret = callBuiltin(i, commands);
+
+    //no process was created, signal the shell to not wait for any process
+    if(ret == 0) {
+        ret = -1;
+    }
+
+    if(ret == -1) {
+        for (int k = 0; k < i; k++) {
+            dealloc((uint64_t)commands[k]);
+        }
+    }
+    dealloc((uint64_t)commands);
+
+    return ret;
 }
 
 void unknownCommand(char* str) {
     printFormat("\nUnknown command:\n\t'%s'\nType 'help' for a list of available commands.\n\n", str);
 }
 
-void help() {
-    for(int i=0;pArr[i]!=null;i++){
+int help(ARGS) {
+    for(int i=0; bArr[i] != null; i++){
         putChar('\n');
-        printFormat("\t'%s': %s \n",pArr[i]->name_id,pArr[i]->description);
+        printFormat("\t'%s': %s \n", bArr[i]->name, bArr[i]->description);
         putChar('\n');
     }
-    exitProc();
+    return 0;
 }
 
-void testException0() {
+int testException0(ARGS) {
    int i=1/0;
-   exitProc();
+   return 0;
 }
 
-void testException6(){
+int testException6(ARGS){
     invalidOp();
-    exitProc();
+    return 0;
 }
 
-void displayTime() {
+int displayTime(ARGS) {
     printTime();
     putChar('\n');
-    exitProc();
+    return 0;
 }
 
-void displayDate() {
+int displayDate(ARGS) {
     printDate();
     putChar('\n');
-    exitProc();
+    return 0;
 }
 
-void playBubbles() {
+int sh(ARGS) {
+    if(argc >= 2) {
+        for(int i=0; pArr[i]!=NULL; i++){
+            if(strcmp(pArr[i]->name, argv[1]) == 0) {
+                return createProcess(pArr[i]->program, HIGH, FOREGROUND, pArr[i]->name, argv + 1);
+            }
+        }
+    }
+    return -1;
+}
+
+int playBubbles(ARGS) {
     enableDoubleBuffering();
     bubbles();
     clearScreen();
     disableDoubleBuffering();
-    exitProc();
+    exitProc(argc, argv);
 }
 
-void playPong() {
+int playPong(ARGS) {
     enableDoubleBuffering();
     pong();
     clearScreen();
     disableDoubleBuffering();
-    exitProc();
+    exitProc(argc, argv);
 }
 
-void playBeep() {
+int playBeep(ARGS) {
     play_beep(2000, 100);
-    exitProc();
+    exitProc(argc, argv);
 }
