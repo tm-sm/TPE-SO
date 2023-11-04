64eae0efe83af5b341181ca50c53a5149b346644
Patricklt2 <ptorlaschis@gmail.com>
2023-11-04 20:25:25 -0300

    done

--- Kernel/include/fdManager.h
+++ Kernel/include/fdManager.h
@@ -14,6 +14,6 @@ void closePipe(int pipeFD);
 
 int redirectPipe(int oldfd, int newfd);
 
-size_t read(int fd, void *buff, size_t bytes);
+size_t read(int pipeFd, char *buff, size_t bytes);
 
-size_t write(int fd, const void * buff, size_t bytes);
+size_t write(int pipeFd, const char * buff, size_t bytes);
