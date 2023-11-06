ffea6724e5ae072850146b7ecd958b8ef132b1be
NCasella <nicolascasella02@gmail.com>
2023-11-06 15:17:21 -0300

    help

--- Userland/SampleCodeModule/philosophers.c
+++ Userland/SampleCodeModule/philosophers.c
@@ -5,7 +5,7 @@
 #define INIT_PHILOSOPHERS 5
 #define HUNGRY 0
 #define EATING 1
-
+#define THINKING 2
 void initPhyloReunion();
 //not HEAVILY inspired by tanenbaum at all :P
 //                                         ^ AI made it
@@ -13,25 +13,22 @@ void initPhyloReunion();
 //aka semaphoreName
 char philosophersName [MAX_PHILOSOPHERS][24]={"Aristoteles","Platon","Socrates","Descartes","Kant","Nietzsche"};
 
-const char * tablemutex="TableMutex";
+static const char tablemutex[]="TableMutex";
 typedef struct philosopher{
     char name[24];
     int state;
     int pid;
 }philosopher;
 
-static int philoAmount=0;
+static int philoAmount=INIT_PHILOSOPHERS;
 typedef struct philosopher* philo;
 
-philo philosophers[MAX_PHILOSOPHERS];
+static philo philosophers[MAX_PHILOSOPHERS];
 
 
-
-
-void philosopherActivity(){
+void philosopherActivity(int argc,char* argv[]){
     waitSem(tablemutex);
     int philoNumber=philoAmount;
-    philoAmount++;
     postSem(tablemutex);
     while(1){
         takeForks(philoNumber);
@@ -40,8 +37,9 @@ void philosopherActivity(){
     }
 }
 
-void initPhyloReunion(int argc,char** argv){
+void initPhyloReunion(int argc,char* argv[]){
     openSem(tablemutex,1);
+    philoAmount=0;
     for(int i=0;i<MAX_PHILOSOPHERS;i++){
         openSem(philosophersName[i],1);
     }
@@ -50,10 +48,12 @@ void initPhyloReunion(int argc,char** argv){
         strcpy(philosophers[i]->name,philosophersName[i]);
         philosophers[i]->state=HUNGRY;
         philosophers[i]->pid=createProcess(philosopherActivity,HIGH,BACKGROUND,0,philosophersName[i],NULL);
+        philoAmount++;
     }
     postSem(tablemutex);
     while(1){
-        for(int i=0;i<INIT_PHILOSOPHERS;i++){
+        waitSem(tablemutex);
+        for(int i=0;i<philoAmount;i++){
             if(philosophers[i]->state==EATING)
                 printFormat("E");
             else
@@ -61,28 +61,30 @@ void initPhyloReunion(int argc,char** argv){
         }
         printFormat("\n");
         wait(1000);
+        postSem(tablemutex);
     }
 }
 
-void takeForks(int i){
+void takeForks(int philoNum){
     waitSem(tablemutex);
-    test(i);
+    philosophers[philoNum]->state=HUNGRY;
+    test(philoNum);
     postSem(tablemutex);
-    waitSem(philosophersName[i]);
+    waitSem(philosophersName[philoNum]);
 }
 
-void putForks(int i){
+void putForks(int philoNum){
     waitSem(tablemutex);
-    philosophers[i]->state=HUNGRY;
-    test(i%philoAmount);
-    test((i+1)%philoAmount);
+    philosophers[philoNum]->state=THINKING;
+    test(philoNum%philoAmount);
+    test((philoNum+1)%philoAmount);
     postSem(tablemutex);
 }
 
-void test(int i){
-    if( philosophers[i]->state!=EATING && philosophers[(i+philoAmount-1)%philoAmount]->state !=EATING && philosophers[(i+1)%philoAmount]->state!=EATING){
-        philosophers[i]->state=EATING;
-        postSem(philosophersName[i]);
+void test(int philoNum){
+    if(philosophers[(philoNum+philoAmount-1)%philoAmount]->state !=EATING && philosophers[(philoNum+1)%philoAmount]->state!=EATING){
+        philosophers[philoNum]->state=EATING;
+        postSem(philosophersName[philoNum]);
     }
 }
 
