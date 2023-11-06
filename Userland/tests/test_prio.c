88272db0a306f822cc0c5ae4ec7a8a5ea8056ccc
Selion11 <gdecaro@itba.edu.ar>
2023-11-06 20:03:03 -0300

    Test push

--- Userland/tests/test_prio.c
+++ Userland/tests/test_prio.c
@@ -1,15 +1,16 @@
 #include <stdint.h>
 #include <stdio.h>
-#include "syscall.h"
+#include "system.h"
+#include "standardLib.h"
 #include "test_util.h"
 
 #define MINOR_WAIT 1000000 // TODO: Change this value to prevent a process from flooding the screen
 #define WAIT 10000000      // TODO: Change this value to make the wait long enough to see theese processes beeing run at least twice
 
 #define TOTAL_PROCESSES 3
-#define LOWEST 0  // TODO: Change as required
+#define LOWEST 2 // TODO: Change as required
 #define MEDIUM 1  // TODO: Change as required
-#define HIGHEST 2 // TODO: Change as required
+#define HIGHEST 0 // TODO: Change as required
 
 int64_t prio[TOTAL_PROCESSES] = {LOWEST, MEDIUM, HIGHEST};
 
@@ -19,33 +20,33 @@ void test_prio() {
   uint64_t i;
 
   for (i = 0; i < TOTAL_PROCESSES; i++)
-    pids[i] = my_create_process("endless_loop_print", 0, argv);
+    pids[i] = createProcess(endless_loop,2,BACKGROUND,0,"endless_loop",argv);
 
   bussy_wait(WAIT);
-  printf("\nCHANGING PRIORITIES...\n");
+  printFormat("\nCHANGING PRIORITIES...\n");
 
   for (i = 0; i < TOTAL_PROCESSES; i++)
-    my_nice(pids[i], prio[i]);
+    setProcessPriority(pids[i], prio[i]);
 
   bussy_wait(WAIT);
-  printf("\nBLOCKING...\n");
+  printFormat("\nBLOCKING...\n");
 
   for (i = 0; i < TOTAL_PROCESSES; i++)
-    my_block(pids[i]);
+    blockProcess(pids[i]);
 
-  printf("CHANGING PRIORITIES WHILE BLOCKED...\n");
+  printFormat("CHANGING PRIORITIES WHILE BLOCKED...\n");
 
   for (i = 0; i < TOTAL_PROCESSES; i++)
-    my_nice(pids[i], MEDIUM);
+    setProcessPriority(pids[i], MEDIUM);
 
   printf("UNBLOCKING...\n");
 
   for (i = 0; i < TOTAL_PROCESSES; i++)
-    my_unblock(pids[i]);
+    unblockProcess(pids[i]);
 
   bussy_wait(WAIT);
-  printf("\nKILLING...\n");
+  printFormat("\nKILLING...\n");
 
   for (i = 0; i < TOTAL_PROCESSES; i++)
-    my_kill(pids[i]);
+   killProcess(pids[i]);
 }
