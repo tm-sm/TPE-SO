0fc6fefc4b61e7668bf0e3817b1b674eff1aaa56
Patricklt2 <ptorlaschis@gmail.com>
2023-11-06 21:19:37 -0300

    cleanup + comments

--- Kernel/include/memTests.h
+++
@@ -1,8 +0,0 @@
-/*
- * Returns 0 if all is okay
- * Returns 1 if there was an error
- * (will print to kernel if something went wrong)
- */
-int memoryProperInitSelfCheck();
-
-
--- Kernel/include/memoryManager.h
+++ Kernel/include/memoryManager.h
@@ -4,16 +4,40 @@
 
 //La memoria se definio en el .c pues donde arranca y termina la memoria no deberia ser visible para tod el so, sino para el MM
 
+/**
+ * Initializes memoryManager
+ * Sets firstBlock to  0x0000000000050000
+ *
+ */
 void createMemoryManager();
 
+/**
+ *
+ * @param size
+ * @return
+ */
 void * allocate(size_t size);
-
+/**
+ *
+ * @param addr
+ */
 void deallocate(void * addr);
-
+/**
+ *
+ * @param ptr
+ * @param newSize
+ * @return
+ */
 void * reallocate(void * ptr, size_t newSize);
 
-size_t convertToPageSize(size_t size, size_t pageSize);
-
+/**
+ *
+ * @return
+ */
 size_t getCurrentMemSize();
 
+/**
+ *
+ * @param pid
+ */
 void deallocateAllProcessRelatedMem(int pid);
